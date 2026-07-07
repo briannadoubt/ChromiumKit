61442bfc6cf1754fc05470ce9e59bb57e696be25
briannadoubt <5713359+briannadoubt@users.noreply.github.com>
2026-07-07 17:15:56 +0000

    Update CEF to 149.0.6+g0d0eeb6+chromium-149.0.7827.201

--- scripts/cef_version.sh
+++ scripts/cef_version.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-readonly CEF_VERSION="148.0.10+g7ee53f5+chromium-148.0.7778.218"
+readonly CEF_VERSION="149.0.6+g0d0eeb6+chromium-149.0.7827.201"
 readonly CEF_DOWNLOAD_BASE_URL="https://cef-builds.spotifycdn.com"
 readonly CEF_ARM64_ARCHIVE="cef_binary_${CEF_VERSION}_macosarm64_minimal.tar.bz2"
 readonly CEF_X64_ARCHIVE="cef_binary_${CEF_VERSION}_macosx64_minimal.tar.bz2"
